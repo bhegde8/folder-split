ae355d7fc1c25e9264b80f3cff9b9f2fb2f3c8c4
Pacnet Netty <bhegde8@gmail.com>
2020-07-21 00:11:59 -0400

    Add material-ui and dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,38 @@
 import { hot } from 'react-hot-loader';
-import * as React from 'react';
+import React, { ReactElement } from 'react';
 
-const App = () => <div>Hi from react !</div>;
+import { AppBar, CssBaseline, ThemeProvider, Toolbar,
+         Typography, Theme } from '@material-ui/core';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { blue } from '@material-ui/core/colors';
+
+// Use the default dark theme provided by Material-UI with blue
+// as the primary color
+const darkMode: Theme = createMuiTheme({
+    palette: {
+        type: 'dark',
+        primary: {
+            main: blue[800],
+        },
+    },
+});
+  
+
+const App = (): ReactElement => (
+    <ThemeProvider theme={darkMode}>
+        <CssBaseline />
+
+        <AppBar>
+            <Toolbar>
+
+                <Typography variant="h5">
+                    Folder Split
+                </Typography>
+
+            </Toolbar>
+        </AppBar>
+
+      </ThemeProvider>
+);
 
 export default hot(module)(App);
